6ff59fcc397418fb46f82397627a19d303e1d5d5
Zachary <zbarryte@mit.edu>
2013-03-28 16:59:21 -0400

    small changes to the receiver, now receives messages from the leapmotionoscsender

--- src/OscReceiverController.cpp
+++ src/OscReceiverController.cpp
@@ -8,6 +8,22 @@
 
 #include "OscReceiverController.h"
 
+//--------------------------------------------------------------
+void OscReceiverController::setup(){
+	// listen on the given port
+//	cout << "listening for osc messages on port " << PORT << "\n";
+	receiver.setup(RELIEF_PORT);
+//    
+//	current_msg_string = 0;
+//	mouseX = 0;
+//	mouseY = 0;
+//	mouseButtonState = "";
+//    
+//	ofBackground(30, 30, 130);
+
+}
+
+//--------------------------------------------------------------
 void OscReceiverController::oscMessageReceived(ofxOscMessage m) {
     ofLog() << "osc message: " << m.getAddress();
 }
--- src/OscReceiverController.h
+++ src/OscReceiverController.h
@@ -8,9 +8,14 @@
 #define EASING_TIME .5f
 #define MIN_EASE_VELOCITY .005f
 
+#define RELIEF_PORT 78746
+
 //--------------------------------------------------------
 class OscReceiverController : public SceneController{
 public:
+    void setup();
     void oscMessageReceived(ofxOscMessage m);
+    
+    ofxOscReceiver receiver;
 };
 
